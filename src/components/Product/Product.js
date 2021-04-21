67d4b6c8d28cf4dbd28f688eccbad4cbf0559e2c
Camila Atencio <camilamatencio@gmail.com>
2021-04-21 00:08:35 -0300

    Fix Categories

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,13 +4,15 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import axios from 'axios';
+import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 
 class Product extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             item: {},
-            price: {}
+            price: {},
+            categories: []
         }
     }
 
@@ -23,31 +25,48 @@ class Product extends React.Component {
                 item: response.data.item,
                 price: response.data.item.price
             });
-            console.log(response)
+            axios.get(`https://api.mercadolibre.com/categories/${response.data.item.category}`)
+            .then(response => {
+                this.setState({
+                    categories: response.data.path_from_root
+                })
+            })
         })
+
     }
 
     render () {
+        const categories = this.state.categories.map(category => {
+            return <Breadcrumbs key={category.id} name={category.name} />
+        })
         return (
-            <Container className="product__container">
-                <Row className="product__row1">
-                    <Col className="product__col1"  md={9}>
-                        <img className="product__image" src={this.state.item.picture} alt=""/>
-                    </Col>
-                    <Col className="product__col2"  md={3}>
-                        <p className="product__p">{this.state.item.condition} - {this.state.item.sold_quantity} vendidos</p>
-                        <h1 className="product__title"><strong>{this.state.item.title}</strong></h1>
-                        <h2 className="product__price">$ {this.state.price.amount}</h2>
-                        <button className="product__button">Comprar</button>
-                    </Col>
-                </Row>
-                <Row className="product__row2">
-                    <Col className="product__col3" md={9}>
-                    <h2 className="product__title2">Descripción del producto</h2>
-                    <p className="product__description">{this.state.item.description}</p>                
-                    </Col>
-                </Row>            
+            <Container>
+                <Container className="breadcrumbs__container">
+                <ul className="breadcrumbs__ul">
+                    {categories}
+                </ul>
+                </Container>
+                <Container className="product__container">
+                    <Row className="product__row1">
+                        <Col className="product__col1"  md={9}>
+                            <img className="product__image" src={this.state.item.picture} alt=""/>
+                        </Col>
+                        <Col className="product__col2"  md={3}>
+                            <p className="product__p">{this.state.item.condition} - {this.state.item.sold_quantity} vendidos</p>
+                            <h1 className="product__title"><strong>{this.state.item.title}</strong></h1>
+                            <h2 className="product__price">$ {this.state.price.amount}</h2>
+                            <button className="product__button">Comprar</button>
+                        </Col>
+                    </Row>
+                    <Row className="product__row2">
+                        <Col className="product__col3" md={9}>
+                        <h2 className="product__title2">Descripción del producto</h2>
+                        <p className="product__description">{this.state.item.description}</p>                
+                        </Col>
+                    </Row>            
+                </Container>                
             </Container>
+
         );        
     };
 };
